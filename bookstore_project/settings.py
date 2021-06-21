e09309ac4b31a8e1c85b2ddfe5edca65dd77b777
Bapi813 <bapimahalik51@hotmail.com>
2021-06-21 20:42:35 +0530

    ch8

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd1db036#-0t0y-298n)+2o-kse!6!kgygprso1lt%l)zxj&$kd'
+#SECRET_KEY = 'd1db036#-0t0y-298n)+2o-kse!6!kgygprso1lt%l)zxj&$kd'
+with open(os.path.join(BASE_DIR,'.txt')) as f:
+    SECRET_KEY= f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
